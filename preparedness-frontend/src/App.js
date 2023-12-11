dc25c5657b11ab3ab7aca0c71d0718b4cbb840d7
rrmorant <duecel@icloud.com>
2023-12-11 00:47:05 -0800

    completed for the most part

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,23 +1,38 @@
-import React from "react"
-import { Button, Input, Label } from "reactstrap"
-import ModalComponent from "./components/ModalComponent"
-import "./App.css"
+import React, { useState } from "react";
+import { Button, Input, Label } from "reactstrap";
+import ModalComponent from "./components/ModalComponent";
+import "./App.css";
 
 const App = () => {
-  return (
-    <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
-      <div className="form">
-        <div className="input">
-          <Label for="name">Enter your name</Label>
-          <Input />
-        </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
-      </div>
-    </div>
-  )
-}
+	const [name, setName] = useState(" ");
 
-export default App
+	const changeInput = (input) => {
+		setName(input.target.value);
+	};
+
+	const buttonReset = () => {
+		setName("");
+	};
+
+	return (
+		<div className="background-image">
+			<div className="entire-content">
+				<h1 className="header">Preparedness Assessment</h1>
+				<div className="form">
+					<div className="input-label">
+						<Label for="name">Enter your name</Label>
+						<Input value={name} onChange={changeInput} />
+					</div>
+					<div className="Button-placement">
+						<ModalComponent userName={name} />
+						<Button className="Button-styling" onClick={buttonReset}>
+							Reset
+						</Button>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default App;
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,42 @@
-import React from "react"
+import React, { useState } from "react";
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
-const ModalComponent = () => {
-  return <></>
-}
+const ModalComponent = ({ userName }) => {
+	const [modal, setModal] = useState(false);
 
-export default ModalComponent
+	const toggle = () => setModal(!modal);
+
+	return (
+		<div>
+			<div className="Button-placement">
+			<Button className="Button-styling" onClick={toggle}>
+				Submit Name
+			</Button>
+      </div>
+			<Modal isOpen={modal} toggle={toggle}>
+				<ModalHeader toggle={toggle}>
+					Well, Hello there {userName}!
+				</ModalHeader>
+				<ModalBody>
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+					aliquip ex ea commodo consequat. Duis aute irure dolor in
+					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+					culpa qui officia deserunt mollit anim id est laborum.
+				</ModalBody>
+				<ModalFooter>
+					<Button color="primary" onClick={toggle}>
+						Close
+					</Button>
+					<Button color="secondary" onClick={toggle}>
+						Cancel
+					</Button>
+				</ModalFooter>
+			</Modal>
+		</div>
+	);
+};
+
+export default ModalComponent;
